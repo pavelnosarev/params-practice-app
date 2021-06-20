3607e33863ea456c31afa62a9c2576a7291a0f9c
“Pavel <itspavelnosarev@gmail.com>
2021-06-20 15:53:23 -0400

    added to app controller

--- app/controllers/user_message_controller.rb
+++ app/controllers/user_message_controller.rb
@@ -1,2 +1,6 @@
 class UserMessageController < ApplicationController
+  def user_message
+    input = params["message"]
+    render json: { message: "#{input}.upcase" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  get "/welcome_path", controller: "welcomes", action: "user_message"
+  get "/user_message", controller: "welcomes", action: "user_message"
 end
